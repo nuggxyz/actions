f613d6c459fbd409c0bda6f61c10a1aea1a270db
walter <iamwalteh@me.com>
2023-06-12 20:15:11 -0500

    upgrade

---
+++ scripts/action_bump.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+formulas=$(find Formula -name "./setup-*/action.yml")
+
+SCRIPT_CHANGELOG="## updated formulas"
+DID_UPDATE=0
+
+for formula in $formulas; do
+
+	file=./$formula
+
+	if [[ ! -f "$file" ]]; then
+		echo "Formula $file not found. Exiting."
+		exit 1
+	fi
+
+	formula=$(dirname "$formula" | cut -d'-' -f2)
+
+	# Get the latest release from GitHub API for the tftab repo
+	LATEST_RELEASE=$(curl --silent "https://api.github.com/repos/nuggxyz/$formula/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+
+	# replace v with nothing
+	LATEST_RELEASE=${LATEST_RELEASE//v/}
+
+	version=$(grep -oE "version \".*\"" "$file" | cut -d\" -f2)
+
+	# Check if the version is already the latest
+	if [[ "${version}" == "${LATEST_RELEASE}" ]]; then
+		echo "Already on latest version ${LATEST_RELEASE}. Exiting."
+	else
+		# Update the yaml file "constant_version: v0.1.2" to the latest release
+		sed -i "s/\(constant_version: \).*/\1${LATEST_RELEASE}/" "$file"
+		echo "Updated $formula to version ${LATEST_RELEASE}."
+		DID_UPDATE=1
+		SCRIPT_CHANGELOG="$SCRIPT_CHANGELOG  \n  \n- $formula: \`${version}\` -> \`${LATEST_RELEASE}\`"
+	fi
+done
+
+echo "DID_UPDATE=$DID_UPDATE" >>"$GITHUB_OUTPUT"
+echo "SCRIPT_CHANGELOG=$SCRIPT_CHANGELOG" >>"$GITHUB_OUTPUT"
