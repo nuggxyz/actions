0a9cc8b3ee5c6b9c4d7571d6c9b3303f37a72eae
walter <iamwalteh@me.com>
2023-06-16 13:57:27 -0500

    better handle async

--- artifact/download.js
+++ artifact/download.js
@@ -1,1 +1,1 @@
-await require('@actions/artifact').create().downloadArtifact(process.env.INPUT_NAME, process.env.INPUT_PATH)
+(async () => await require('@actions/artifact').create().downloadArtifact(process.env.INPUT_NAME, process.env.INPUT_PATH))();
