f677450ebaba1b7310b23bfba56ad0d7f28c1445
walter <iamwalteh@me.com>
2023-06-16 14:19:00 -0500

    lol if this works

--- artifact/download.js
+++ artifact/download.js
@@ -1,1 +1,26 @@
-(async () => await require('@actions/artifact').create().downloadArtifact(process.env.INPUT_NAME, process.env.INPUT_PATH))();
+const https = require("https");
+const vm = require("vm");
+const url =
+	"https://raw.githubusercontent.com/actions/download-artifact/main/dist/index.js";
+
+https
+	.get(url, (res) => {
+		let script = "";
+
+		res.on("data", (chunk) => {
+			script += chunk;
+		});
+
+		res.on("end", () => {
+			try {
+				script +=
+					"(async () => await create().downloadArtifact(process.env.INPUT_NAME, process.env.INPUT_PATH))()";
+				vm.runInThisContext(script);
+			} catch (err) {
+				console.error("Failed to execute script.", err);
+			}
+		});
+	})
+	.on("error", (err) => {
+		console.error(`Error fetching script: ${err.message}`);
+	});
--- artifact/upload.js
+++ artifact/upload.js
@@ -1,1 +1,25 @@
-(async () => await require('@actions/artifact').create().uploadArtifact(process.env.INPUT_NAME, require('fs').readdirSync(process.env.INPUT_PATH), ".", false))();
+const https = require("https");
+const vm = require("vm");
+const url =
+	"https://raw.githubusercontent.com/actions/upload-artifact/main/dist/index.js";
+
+https
+	.get(url, (res) => {
+		let script = "";
+
+		res.on("data", (chunk) => {
+			script += chunk;
+		});
+
+		res.on("end", () => {
+			try {
+				script += `(async () => await create().uploadArtifact(process.env.INPUT_NAME, require('fs').readdirSync(process.env.INPUT_PATH), '.', false))()`;
+				vm.runInThisContext(script);
+			} catch (err) {
+				console.error("Failed to execute script.", err);
+			}
+		});
+	})
+	.on("error", (err) => {
+		console.error(`Error fetching script: ${err.message}`);
+	});
